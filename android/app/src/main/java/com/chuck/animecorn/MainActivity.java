e53a676f5d080bdea98fcdea81d01a2302a5d49d
andrianvillarosa36-max <andrianvillarosa36@gmail.com>
2026-06-29 03:38:03 +0800

    add NativeBridge for native orientation control

--- android/app/src/main/java/com/chuck/animecorn/MainActivity.java
+++ android/app/src/main/java/com/chuck/animecorn/MainActivity.java
@@ -1,5 +1,51 @@
 package com.chuck.animecorn;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inject native bridge after WebView is ready
+        getBridge().getWebView().addJavascriptInterface(new NativeBridge(), "NativeBridge");
+    }
+
+    public class NativeBridge {
+
+        @JavascriptInterface
+        public void lockLandscape() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+            });
+        }
+
+        @JavascriptInterface
+        public void unlockOrientation() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                }
+            });
+        }
+
+        @JavascriptInterface
+        public void lockPortrait() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }
+            });
+        }
+    }
+}
